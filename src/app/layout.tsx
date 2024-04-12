4ffcf8ce785ae6cb37b999e864a5292c792de93c
cherrydub <cherrydub@gmail.com>
2024-04-12 14:25:03 +0100

    adding a fixed div within the header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           className="bg-[#dbd7fb] absolute top-[-1rem] -z-10  left-[-35rem]
          h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
         ></div>
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="z-100 relative h-[5000px]">
+      <div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blue-[0.5rem] sm:top-6 sm:h-[4.25rem] sm:w-[36rem] sm:rounded-full"></div>
+    </header>
+  );
+}
