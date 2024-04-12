e7a2f312df1999e6c8f3a4e9161d5e6564f9feeb
cherrydub <cherrydub@gmail.com>
2024-04-12 14:53:34 +0100

    since this wont have multiple pages/routes, I have decided to take out the src folder

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,8 @@
 
 import { motion } from "framer-motion";
 import React from "react";
+import Link from "next/link";
+import { links } from "@/lib/data";
 
 export default function Header() {
   return (
@@ -12,6 +14,22 @@ export default function Header() {
         animate={{ y: 0, x: "-50%", opacity: 1 }}
         transition={{ duration: 0.5 }}
       ></motion.div>
+
+      <nav
+        className="fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem]
+      sm:h-[initial] sm:py-0"
+      >
+        <ul>
+          {links.map((link, index) => (
+            <li
+              key={link.hash}
+              className="relative flex items-center gap-1 sm:gap-2"
+            >
+              <Link href={link.hash}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
