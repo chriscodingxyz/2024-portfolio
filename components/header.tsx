7b704e7c6c911dd17ba19e365d04e48c013222cb
cherrydub <cherrydub@gmail.com>
2024-04-12 18:13:13 +0100

    using framer motion to transition the background span amongst active links

--- components/header.tsx
+++ components/header.tsx
@@ -49,7 +49,15 @@ export default function Header() {
               >
                 {link.name}
                 {link.name === activeSection && (
-                  <span className="bg-gray-100 rounded-full absolute inset-0 -z-10"></span>
+                  <motion.span
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10"
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
                 )}
               </Link>
             </motion.li>
