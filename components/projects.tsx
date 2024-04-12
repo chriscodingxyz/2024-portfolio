f45692fa0562528db46f4e5d07ad24d62489fc10
cherrydub <cherrydub@gmail.com>
2024-04-12 17:32:31 +0100

    seperated projectcard into its own component, will begin using useScroll along with useRef for animation while users scroll

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,49 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Image from "next/image";
+import { useScroll } from "framer-motion";
+import ProjectsCard from "./projects-card";
 
-export default function ProjectsComponent() {
+export default function Projects() {
   return (
     <section>
       <SectionHeading>My Projects</SectionHeading>
       <div className="grid gap-6 md:grid-cols-2">
         {projectsData.map((project) => (
           <React.Fragment key={project.title}>
-            <Project {...project} />
+            <ProjectsCard {...project} />
           </React.Fragment>
         ))}
       </div>
-    </section>
-  );
-}
-//since we're inporting projectsData from lib/data.ts as a const, we can use that type
-type ProjectProps = (typeof projectsData)[number];
-
-function Project({ title, description, tags, imageUrl }: ProjectProps) {
-  return (
-    <section className=" group relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8">
-      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col  h-full group-even:ml-[26rem]">
-        <h3 className="text-2xl font-semibold">{title}</h3>
-        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-        <ul className="flex flex-wrap gap-2 mt-4 sm:mt-auto">
-          {tags.map((tag) => (
-            <li
-              key={tag}
-              className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <Image
-        src={imageUrl}
-        alt={title}
-        quality={95}
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40 "
-      />
     </section>
   );
 }
