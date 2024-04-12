5417553e8da91958f639975a15b14bd8f59e6d69
cherrydub <cherrydub@gmail.com>
2024-04-12 17:47:49 +0100

    finished up styling ProjectsCard with correct grouping

--- components/projects-card.tsx
+++ components/projects-card.tsx
@@ -26,10 +26,10 @@ export default function ProjectsCard({
   return (
     <motion.div
       style={{ scale: scaleProgress, opacity: opacityProgress }}
-      className="group mb-3 sm:mb-8 last:mb-0"
+      className="group mb-3 sm:mb-8 last:mb-0 "
       ref={ref}
     >
-      <section className="  relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem]  even:pl-8 hover:bg-gray-200 transition">
+      <section className="rounded-lg  relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem] group-even:pl-8  hover:bg-gray-200 transition">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col  h-full group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
@@ -49,7 +49,7 @@ export default function ProjectsCard({
           src={imageUrl}
           alt={title}
           quality={95}
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40  group-hover:-translate-x-3 group-hover:translate-y-3 
+          className=" absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40  group-hover:-translate-x-3 group-hover:translate-y-3 
         group-hover:-rotate-2 transition
         group-hover:scale-[1.04]
         group-even:group-hover:translate-x-3 
