3f2b4c9c2cc120aa059abfc3968d7d0f573ca70d
cherrydub <cherrydub@gmail.com>
2024-04-15 12:29:04 +0100

    added motion.section to show opacity within 1 second while in view

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Contact() {
+  const { ref } = useSectionInView("Contact");
+
   return (
-    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%, 35rem)]">
+    <motion.section
+      ref={ref}
+      id="contact"
+      className="mb-20 sm:mb-28 w-[min(100%, 35rem)] text-center"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+    >
       <SectionHeading>Contact</SectionHeading>
       <p className="text-gray-700">
         Please contact me directly @
@@ -15,22 +29,22 @@ export default function Contact() {
         <input
           type="email"
           className="h-14 rounded-lg border border-black/10 px-3"
-          placeholder="Email"
+          placeholder="Your Email"
         />
         <textarea
           className="h-52 my-3 rounded-lg border border-black/10 p-3"
           name=""
           id=""
-          placeholder="Message"
+          placeholder="Your Message"
         ></textarea>
         <button
-          className="flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950"
           type="submit"
         >
           Submit
-          <FaPaperPlane className="text-xs opacity-70 transition-all" />
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
         </button>
       </form>
-    </section>
+    </motion.section>
   );
 }
