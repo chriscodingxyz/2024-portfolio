5d7f619856353ca39bf38358094b1c13b18d7347
cherrydub <cherrydub@gmail.com>
2024-04-15 17:35:25 +0100

    decreased threshold for my projects.tsx, this way it will update correctly in the navigation as its scrolled past

--- components/projects.tsx
+++ components/projects.tsx
@@ -7,7 +7,7 @@ import ProjectsCard from "./projects-card";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
-  const { ref } = useSectionInView("Projects", 0.5);
+  const { ref } = useSectionInView("Projects", 0.25);
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My Projects</SectionHeading>
